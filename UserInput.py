d7c759c13c46653f31489e61277b8883472a71a1
plugkevo <munenekevin366@gmail.com>
2024-07-01 13:22:21 +0300

    first on pycharm

---
+++ UserInput.py
@@ -0,0 +1,15 @@
+x = int(input("Enter value"))
+y = int(input("Enter 2nd value"))
+z = x + y
+print(z)
+
+#how to print a character
+ch = input('input a char')
+print(ch[0])
+
+#how to print a character
+ch = input('input a char')[0]
+print(ch)
+
+result = eval(input("enter an expression"))
+print(result)
---
+++ new.py
@@ -0,0 +1,4 @@
+#to not import everything
+from math import sqrt, pow
+y= sqrt(81)
+print(y)
